963777879a9ef10ca9c3e5458681abaa8b8ce509
Bogdan Chulakov <49172210+BogdanChulakov@users.noreply.github.com>
2026-06-05 00:58:40 +0300

    add sounds

--- src/app/components/watch-component/watch-component.ts
+++ src/app/components/watch-component/watch-component.ts
@@ -10,52 +10,61 @@ import { map, switchMap, startWith } from 'rxjs/operators';
   imports: [CommonModule, FormsModule],
   templateUrl: './watch-component.html',
   styleUrl: './watch-component.css'
-  })
+})
 export class WatchComponent implements OnInit, OnDestroy {
   currentTime$!: Observable<Date>;
   
-  // Полета за въвеждане от потребителя
   prepareTimeInput: number = 5;
   mainTimeInput: number = 3;
 
-  // Логически състояния за интерфейса
   isPreparing: boolean = false;
   isPaused: boolean = false;
-  isResultShown: boolean = false; // Показва дали таймерът е пуснат изобщо
+  isResultShown: boolean = false;
 
-  // Вътрешни променливи за пресмятане на времето
   private totalTicks: number = 0;
   private currentTick: number = 0;
-
-  // Контролира състоянието на таймера (true = работи, false = пауза)
   private isRunning$ = new BehaviorSubject<boolean>(false);
-  
-  // Публична променлива за HTML дисплея
   displayValue: string = '03:00';
-  
   private timerSubscription!: Subscription;
 
-  ngOnInit() {
-    // Часовникът в реално време
-    this.currentTime$ = interval(1000).pipe(
-      startWith(0),
-      map(() => new Date())
-    );
+  // --- ДЕФИНИРАНЕ НА ЗВУЦИТЕ ---
+  // 1. Повтарящ се звук за подготовка (ще го пуснем и ще свири през цялото време)
+  private prepareTickSound = new Audio('https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg');
+  
+  // 2. Единична чиста камбана/гонг за старта на таймера
+ private startBellSound = new Audio('https://actions.google.com/sounds/v1/transportation/ship_bell.ogg');
+  
+  // 3. Единична камбана/сигнал за финала
+  private endBellSound = new Audio('https://actions.google.com/sounds/v1/transportation/ship_bell.ogg');
 
-    // Главната RxJS машина, която управлява брояча и поддържа Пауза
-    this.timerSubscription = this.isRunning$.pipe(
-      switchMap(running => {
-        // Ако running е true -> тиктака на всяка секунда, ако е false -> спира (NEVER)
-        return running ? interval(1000) : NEVER;
-      })
-    ).subscribe(() => {
-      this.tick();
-    });
+ngOnInit() {
+  // 1. Настройваме силата на звука на всички ефекти на максимум (1.0)
+  this.prepareTickSound.volume = 1.0;
+  this.startBellSound.volume = 1.0;
+  this.endBellSound.volume = 1.0;
 
-    this.updateDefaultDisplay();
-  }
+  // 2. Зареждаме новите кратки звуци предварително в паметта на браузъра
+  this.prepareTickSound.load();
+  this.startBellSound.load();
+  this.endBellSound.load();
 
-  // Обновява времето на екрана, докато потребителят пише в полетата (преди старт)
+  // 3. Часовникът в реално време (най-отгоре)
+  this.currentTime$ = interval(1000).pipe(
+    startWith(0),
+    map(() => new Date())
+  );
+
+  // 4. Главната RxJS машина, която управлява старта и паузата
+  this.timerSubscription = this.isRunning$.pipe(
+    switchMap(running => running ? interval(1000) : NEVER)
+  ).subscribe(() => {
+    this.tick();
+  });
+
+  // 5. Инициализиране на първоначалния дисплей
+  this.updateDefaultDisplay();
+}
+
   updateDefaultDisplay() {
     if (!this.isResultShown) {
       this.displayValue = `${this.mainTimeInput.toString().padStart(2, '0')}:00`;
@@ -64,13 +73,15 @@ export class WatchComponent implements OnInit, OnDestroy {
 
   startTimer() {
     if (this.isPaused) {
-      // Ако е бил на пауза, просто го възобновяваме
       this.isPaused = false;
       this.isRunning$.next(true);
+      // Ако сме паузирали по време на подготовка, пускаме звука пак
+      if (this.isPreparing) {
+        this.prepareTickSound.play().catch(err => console.log(err));
+      }
       return;
     }
 
-    // Инициализиране на чисто нов таймер
     const totalStartSeconds = this.mainTimeInput * 60;
     this.totalTicks = this.prepareTimeInput + totalStartSeconds;
     this.currentTick = 0;
@@ -79,26 +90,72 @@ export class WatchComponent implements OnInit, OnDestroy {
     this.isPaused = false;
     this.isPreparing = this.prepareTimeInput > 0;
 
-    // Първоначално изписване веднага при клик (0-вата секунда)
-    this.calculateDisplay(this.currentTick);
-    
-    // Стартираме тиктакането
-    this.isRunning$.next(true);
-  }
-
-  // Изпълнява се на всяка изминала секунда
-  private tick() {
-    this.currentTick++;
-
-    if (this.currentTick > this.totalTicks) {
-      this.stopTimer(); // Времето свърши
-      return;
+    // СТАРТ НА ПОДГОТОВКАТА: Пускаме звука да свири непрекъснато
+    if (this.isPreparing) {
+      this.prepareTickSound.currentTime = 0;
+      this.prepareTickSound.play().catch(err => console.log(err));
+    } else {
+      // Ако потребителят е избрал 0 секунди подготовка, директно удряме стартовата камбана
+      this.playSound(this.startBellSound);
     }
 
     this.calculateDisplay(this.currentTick);
+    this.isRunning$.next(true);
   }
 
-  // Логиката за пресмятане какво да се покаже
+private tick() {
+  this.currentTick++;
+
+  // 1. Докато сме във фаза подготовка, пускаме краткото пиукане
+  if (this.currentTick < this.prepareTimeInput) {
+    this.playSound(this.prepareTickSound);
+  } 
+  
+  // 2. ХВАЩАМЕ ТОЧНИЯ МОМЕНТ: Подготовката свърши, започва рундът!
+  else if (this.currentTick === this.prepareTimeInput) {
+    // СТОП НА ПРЕПАРЕ: Спираме и зануляваме подготвителния звук веднага, за да не прелива в рунда!
+    this.prepareTickSound.pause();
+    this.prepareTickSound.currentTime = 0;
+
+    // Пускаме камбаната за старт на рунда
+    this.playSound(this.startBellSound);
+  }
+
+  // 3. ФИНАЛ: Когато времето изтече напълно (00:00)
+  if (this.currentTick > this.totalTicks) {
+    this.isRunning$.next(false);
+    this.isResultShown = false;
+    
+    // Спираме всичко останало за всеки случай
+    this.prepareTickSound.pause();
+    this.startBellSound.pause();
+
+    // Пускаме финалната камбана
+    this.playSound(this.endBellSound);
+    
+    this.updateDefaultDisplay();
+    return;
+  }
+
+  this.calculateDisplay(this.currentTick);
+}
+
+  private playSound(audio: HTMLAudioElement) {
+    audio.currentTime = 0;
+    audio.play().catch(err => console.log('Аудиото е блокирано:', err));
+  }
+
+  private stopAllSounds() {
+    this.prepareTickSound.pause();
+    this.prepareTickSound.currentTime = 0;
+    
+    this.startBellSound.pause();
+    this.startBellSound.currentTime = 0;
+    
+    this.endBellSound.pause();
+    this.endBellSound.currentTime = 0;
+  }
+
   private calculateDisplay(tick: number) {
     if (tick < this.prepareTimeInput) {
       this.isPreparing = true;
@@ -118,7 +175,8 @@ export class WatchComponent implements OnInit, OnDestroy {
 
   pauseTimer() {
     this.isPaused = true;
-    this.isRunning$.next(false); // Спира тиктакането, но пази текущата секунда
+    this.isRunning$.next(false);
+    this.prepareTickSound.pause(); // Паузираме фоновия звук, ако спрем по време на подготовка
   }
 
   stopTimer() {
@@ -126,19 +184,19 @@ export class WatchComponent implements OnInit, OnDestroy {
     this.isPaused = false;
     this.isPreparing = false;
     this.isResultShown = false;
+    this.stopAllSounds();
     this.updateDefaultDisplay();
   }
 
   restartTimer() {
     this.stopTimer();
-    // Изчакваме минимално време Angular да занули състоянието и стартираме наново
     setTimeout(() => this.startTimer(), 50);
   }
 
   ngOnDestroy() {
-    // Важно за предотвратяване на memory leaks
     if (this.timerSubscription) {
       this.timerSubscription.unsubscribe();
     }
+    this.stopAllSounds();
   }
 }
